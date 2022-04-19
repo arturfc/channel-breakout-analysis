9eca185dd5d6d90a0c11610211c5226e7c752dff
Artur Cunha <arturfernandescunha@hotmail.com>
2022-04-19 13:31:57 -0300

    adding parquet file

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import MetaTrader5 as mt5
 import pandas as pd
 import time
 import pytz
-from datetime import datetime
+import plotly.graph_objects as go
 import cufflinks as cf
 import numpy as np
+import seaborn as sns
+import pyarrow.parquet as pq
+from datetime import datetime
 from scipy.signal import argrelextrema
 from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+
 
 cf.set_config_file(offline = True)
 
@@ -19,14 +22,19 @@ if not mt5.initialize():
 # %%
 symbol = 'WDO@N'
 
-dateBegin =  datetime(2022,3,28,0,0)
-dateEnd = datetime(2022,4,2,0)
+'''dateBegin =  datetime(2022,4,18,0,0)
+dateEnd = datetime(2022,4,19,0)
 
 rates_df = pd.DataFrame(mt5.copy_rates_range(symbol, mt5.TIMEFRAME_M1, dateBegin, dateEnd))
+rates_df_2 = rates_df
 rates_df['time'] = pd.to_datetime(rates_df['time'], unit='s')
+
+rates_df.set_index('time', inplace=True)'''
+
+rates_df = pq.ParquetFile("output.parquet").read().to_pandas()
 rates_df.set_index('time', inplace=True)
-#rates_df.iplot(kind='candle')
-#rates_df.time
+rates_df.index = pd.to_datetime(rates_df.index, format="%Y-%m-%d %H:%M:%S")
+
 
 # %%
 #Armazenando high locais
@@ -64,10 +72,10 @@ value_per_pip = 5
 order_tax = 0
 trade_volume = 1
 
-extra_range_entry = 2
+extra_range_entry = 0.5
 n_bars_validation = 5
 entry_threshold_attemp = 30
-operation_duration = 15
+operation_duration = 5
 order_to_be_filled_threshold = 30
 
 trade_hour_threshold = 17
@@ -79,6 +87,9 @@ df_["local_position_close"] = 0
 df_["entry_value"] = 0
 total_entries = 0
 profit = []
+
+ignored_pullbacks = 0
+ignored_limit_orders = 0
 for i in range(1,len(local_max_index)):
 
   #Permitindo apenas canais diários
@@ -98,14 +109,14 @@ for i in range(1,len(local_max_index)):
   limitOrderPrice = 0
   orderRunner=0
 
-  print("i = ", i)
+  #print("i = ", i)
   #corrigir tamanho max desse for para menos de 540 min
   for close in df_[(local_max_index[i]+1):len(df_)].close.values:
     j += 1
     #Não pode ultrapassar horário limite
     if (df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.hour[0]) >= trade_hour_threshold and (df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.minute[0]) > trade_minute_treshold:
-      print('Time to trade is over', df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
-      print('OrdersTotal:', ordersTotal, 'PositionsTotal:', positionsTotal)
+      #print('Time to trade is over', df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
+      #print('OrdersTotal:', ordersTotal, 'PositionsTotal:', positionsTotal)
       break
 
     lineVariation = (barDistance_pips*(barDistance_range+j))/barDistance_range
@@ -113,8 +124,8 @@ for i in range(1,len(local_max_index)):
 
     #Verificando breakout
     if(close > breakOutLine and calculatingPullbackEntry == False):
-      #print("Break out bar at number", (df_[(local_max_index[2]+j):(local_max_index[2]+j+1)].i).values[0])
-      print("Break out price:", close, "at", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
+      #--------------
+      #print("Break out price:", close, "at", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
       calculatingPullbackEntry = True
       j += (n_bars_validation-2)
 
@@ -127,8 +138,7 @@ for i in range(1,len(local_max_index)):
 
         #Se ativar ordem, cai aqui
         if df_[(local_max_index[i]+j):(local_max_index[i]+j+1)].low.values[0] <= limitOrderPrice:
-          #print(df_[(local_max_index[i]+j):(local_max_index[i]+j+1)].low.values[0], "<=", limitOrderPrice)
-          print("Position created at", limitOrderPrice, "at", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0], "limitOrder:", limitOrderPrice)
+          #print("Position created at", limitOrderPrice, "at", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0], "limitOrder:", limitOrderPrice)
           positionsTotal = True  #plotar entrada no grafico
           total_entries += 1
           df_['local_entry'][(local_max_index[i]+j):(local_max_index[i]+j+1)] = 1
@@ -139,33 +149,48 @@ for i in range(1,len(local_max_index)):
           #calcular distância do profit
           profit.append(((df_[(local_max_index[i]+j):(local_max_index[i]+j+1)].close.values - limitOrderPrice)*value_per_pip*trade_volume)+order_tax)
           #
-          print("Exit point:", df_[(local_max_index[i]+j):(local_max_index[i]+j+1)].close.values[0], df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
+          #print("Exit point:", df_[(local_max_index[i]+j):(local_max_index[i]+j+1)].close.values[0], df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
           break
 
         #Ordem limite precisa ser acionada em menos de x minutos
         if (orderRunner > order_to_be_filled_threshold):
-          print("Order couldn't be filled.", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
+          ignored_limit_orders += 1
+          #print("Order couldn't be filled.", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
           break
 
       #Validando pullback (breakout validado)
       elif (ordersTotal == False):
 
         if(df_[(local_max_index[i]+j):(local_max_index[i]+j+1)].low.values[0] > breakOutLine):
+          #Abrindo ordem limite
           limitOrderPrice = breakOutLine + extra_range_entry
           limitOrderPrice = round_limit_order_price(limitOrderPrice)
           ordersTotal = True 
-          print("Opening first limit order at",limitOrderPrice, "limitOrder:", limitOrderPrice, "at", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
+          #print("Opening first limit order at",limitOrderPrice, "limitOrder:", limitOrderPrice, "at", df_[(local_max_index[i]+j):(local_max_index[i]+1+j)].index.time[0])
         else:
-          print("Pullback ignored")
+          #Ignorando pullback
+          ignored_pullbacks += 1
+          #print("Pullback ignored")
           break
 
 
 #----------Up -> inside for
-print("Total entries:", total_entries)
-print(profit)
+profit = pd.DataFrame(profit)
+print(sns.distplot(profit, bins=15))
+print("Total entries:", total_entries,
+      "\nIgnored pullbacks:", ignored_pullbacks,
+      "\nIgnored limit orders:", ignored_limit_orders,
+      "\nMean:", profit[0].mean(),
+      "\nStd:", profit[0].std(),)
+
+
 
 # %%
 #Plotando gráfico: exibindo max locais, entradas e saídas
+
+dateBegin =  datetime(2022,3,31,0,0)
+dateEnd = datetime(2022,4,1,0)
+
 df_2 = df_[(df_.index >= dateBegin) & (df_.index <= dateEnd)].copy()
 
 fig = make_subplots(rows=2, cols=1, shared_xaxes=True, specs=[[{"secondary_y": False}], [{"secondary_y": True}]],
